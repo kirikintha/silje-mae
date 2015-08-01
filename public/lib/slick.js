35a5250e910ec13d630310f198d0af7c8d6ac578
Paul Huntsberger <paul@303software.com>
2015-08-01 09:25:30 -0600

    carousel debug

--- public/lib/slick.js
+++ public/lib/slick.js
@@ -52,7 +52,6 @@ angular.module('slick', []).directive('slick', [
                 nextArrow: '@'
             },
             link: function(scope, element, attrs) {
-                console.debug(scope);
                 var destroySlick, initializeSlick, isInitialized;
                 destroySlick = function() {
                     return $timeout(function() {
