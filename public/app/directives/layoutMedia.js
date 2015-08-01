62426dd2e55fadc1d776bf02674ef00c45fcab63
Paul Huntsberger <paul@303software.com>
2015-08-01 08:45:57 -0600

    more updates to layout controller

--- public/app/directives/layoutMedia.js
+++ public/app/directives/layoutMedia.js
@@ -1,11 +1,20 @@
 //Photo Layout.
 angular.module('MediaApp.directives')
-        .directive('layoutMedia', function () {
-            return {
-                restrict: 'E',
-                replace: true,
-                templateUrl: function (elem, attr) {
-                    return '/partials/media/layout.' + attr.type + '.html';
+    .directive('layoutMedia', ['$timeout', function($timeout) {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: function(elem, attr) {
+                return '/partials/media/layout.' + attr.type + '.html';
+            },
+            controller: function($scope) {
+                //If our layout is carousel, then attach the carousel before change event.
+                if ($scope.layout === 'carousel') {
+                    $timeout(function() {
+                        var carousel = angular.element('.slider-nav, .slider-for');
+                        console.debug(carousel);
+                    }, 0);
                 }
-            };
-        });
+            }
+        };
+    }]);
